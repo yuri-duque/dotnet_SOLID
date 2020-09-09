b28952ca78957c6c72bc909f2c5578346f7c7106
Yuri Duque <yurithielmann83@gmail.com>
2020-09-08 22:31:43 -0300

    adicionando arquivos single responsibility

---
+++ SOLID/Single Responsibility/Right.cs
@@ -0,0 +1,20 @@
+﻿namespace SOLID.Single_Responsibility
+{
+    // https://medium.com/backticks-tildes/the-s-o-l-i-d-principles-in-pictures-b34ce2f1e898
+
+    // Single Responsibility
+
+    // A class should have a single responsibility
+
+    //If a Class has many responsibilities, it increases the possibility of bugs
+    // because making changes to one of its responsibilities, could affect the 
+    //other ones without you knowing.
+
+    //Goal
+    //This principle aims to separate behaviours so that if bugs arise as a 
+    //result of your change, it won’t affect other unrelated behaviours.
+
+    public class Right
+    {
+    }
+}
